6262315b206721cdd5964739feeffce6cf963841
Subhiddin <astrum@iMac-astrum-123.local>
2024-07-02 15:31:12 +0500

    Setting up the project

---
+++ TikTok Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok Clone
+//
+//  Created by Subhiddin  on 02/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
